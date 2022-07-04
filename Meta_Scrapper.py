437e3eba2c7b0ebd50f88e7fd8e7b0c75560e481
DhruvAgarwal120 <77245785+DhruvAgarwal120@users.noreply.github.com>
2022-07-04 13:56:30 +0530

    Update Meta_Scrapper.py

--- Meta_Scrapper.py
+++ Meta_Scrapper.py
@@ -1,1 +1,43 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+DRIVER_PATH = r"C:\Users\91939\Desktop\chromedriver.exe"
+
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+
+driver.get("https://www.metacareers.com/jobs")
+
+driver.implicitly_wait(8)
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
+
+Entire_Element = soup.find("div", class_ = '_8tk7')
+
+jobTitle = Entire_Element.find_all("div", class_='_8sel _97fe')
+
+JobTitle = []
+for titles in jobTitle:
+    JobTitle.append(titles.text)
+print(JobTitle)
+
+LocationCategoryOverallJob = Entire_Element.find_all("div", class_='_8see _97fe')
+
+LocationsCategoryOverallJobs = []
+for loc in LocationCategoryOverallJob:
+    LocationsCategoryOverallJobs.append(loc.text)
+print(LocationsCategoryOverallJobs)
+
+Locations = []
+Locations.append(LocationsCategoryOverallJobs[0::3])
+print(Locations)
+
+Category = []
+Category.append(LocationsCategoryOverallJobs[1::3])
+print(Category)
+
+OverallJob = []
+OverallJob.append(LocationsCategoryOverallJobs[2::3])
+print(OverallJob)
+
+
 
