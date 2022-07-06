786d0025e68c889f44723e6ef264670f7931c43d
DhruvAgarwal120 <77245785+DhruvAgarwal120@users.noreply.github.com>
2022-07-07 02:08:54 +0530

    Update Meta_Scrapper.py

--- Meta_Scrapper.py
+++ Meta_Scrapper.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
+
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://akahinternship:akahinternship@cluster0.wvqnd.mongodb.net/?retryWrites=true&w=majority")
+db = client["jobs"]
+collection = db["joblist"]
 
 DRIVER_PATH = r"C:\Users\91939\Desktop\chromedriver.exe"
 
@@ -7,7 +13,7 @@ driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
 driver.get("https://www.metacareers.com/jobs")
 
-driver.implicitly_wait(8)
+time.sleep(5)
 
 soup = BeautifulSoup(driver.page_source, "html.parser")
 
@@ -18,30 +24,29 @@ jobTitle = Entire_Element.find_all("div", class_='_8sel _97fe')
 JobTitle = []
 for titles in jobTitle:
     JobTitle.append(titles.text)
-print(JobTitle)
 
 LocationCategoryOverallJob = Entire_Element.find_all("div", class_='_8see _97fe')
 
 LocationsCategoryOverallJobs = []
 for loc in LocationCategoryOverallJob:
     LocationsCategoryOverallJobs.append(loc.text)
-print(LocationsCategoryOverallJobs)
 
 Locations = []
 Locations.append(LocationsCategoryOverallJobs[0::3])
-print(Locations)
 
 Category = []
 Category.append(LocationsCategoryOverallJobs[1::3])
-print(Category)
 
 OverallJob = []
 OverallJob.append(LocationsCategoryOverallJobs[2::3])
-print(OverallJob)
 
-# This only prints the URL after "https://www.metacareers.com/jobs". Need to add this URL after the meta url in the database!
 URL = Entire_Element.find_all('a', class_ = '_8sef')
 JURL = []
 for links in URL:
     JURL.append(links.get("href"))
 
+for i in range(0, len(JobTitle)):
+    post = {"company":"Meta","Job_Title":JobTitle[i], "Job_Category":Category[0][i], "Job_Location":Locations[0][i], "Job_Qualifications":"~", "Job_Link":"metacareers.com" + JURL[i]}
+    collection.insert_one(post)
+
+driver.quit()
