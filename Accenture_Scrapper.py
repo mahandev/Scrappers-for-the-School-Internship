ce488ede35814de8fd7cfdfcac61aee4b8620009
DhruvAgarwal120 <77245785+DhruvAgarwal120@users.noreply.github.com>
2022-07-07 01:52:04 +0530

    Update Accenture_Scrapper.py

--- Accenture_Scrapper.py
+++ Accenture_Scrapper.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
+
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://akahinternship:akahinternship@cluster0.wvqnd.mongodb.net/?retryWrites=true&w=majority")
+db = client["jobs"]
+collection = db["joblist"]
 
 DRIVER_PATH = r"C:\Users\91939\Desktop\chromedriver.exe"
 
@@ -7,7 +13,7 @@ driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
 driver.get("https://www.accenture.com/in-en/careers/jobsearch?src=inFY22pscgoogle&")
 
-driver.implicitly_wait(8)
+time.sleep(5)
 
 soup = BeautifulSoup(driver.page_source, "html.parser")
 
@@ -19,7 +25,6 @@ Locations = []
 for loc in Location:
     Locations.append(loc.text)
 
-print(Locations)
 
 jobTitle = Entire_Job.find_all("a")
 
@@ -27,7 +32,6 @@ JobTitles = []
 for titles in jobTitle:
     JobTitles.append(titles.text)
 
-print(JobTitles)
 
 OverallJob = Entire_Job.find_all("span", class_ = 'areas-of-interest corporate-semibold')
 
@@ -35,7 +39,6 @@ OverallJobs = []
 for jobs in OverallJob:
     OverallJobs.append(jobs.text)
 
-print(OverallJobs)
 
 BusinessArea = Entire_Job.find_all("p", class_ = 'job-business-area corporate-regular')
 
@@ -43,7 +46,6 @@ BAreas = []
 for areas in BusinessArea:
     BAreas.append(areas.text)
 
-print(BAreas)
 
 jobLink = Entire_Job.find_all('a')
 
@@ -51,4 +53,7 @@ JLinks = []
 for links in jobLink:
     JLinks.append(links.get("href"))
 
-print(JLinks)
+
+for i in range(0, len(JobTitles)):
+    post = {"company":"Accenture","Job_Title": JobTitles[i], "Job_Category":OverallJobs[i], "Job_Location":Locations[i], "Job_Qualifications":"~", "Job_Link":JLinks[i]}
+    collection.insert_one(post)
